6c083e7e2989924b8dfdea3fd560cc5a8464a9e2
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2021-09-08 02:40:09 -0500

    message successfull to form products create and redirection /home

--- FrontEnd/cliente/src/app/app.module.ts
+++ FrontEnd/cliente/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app.component';
 import { ProductCreateComponent } from './components/product-create/product-create.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
@@ -16,7 +18,9 @@ import { ProductsListComponent } from './components/products-list/products-list.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FrontEnd/cliente/src/app/components/product-create/product-create.component.ts
+++ FrontEnd/cliente/src/app/components/product-create/product-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-create',
@@ -9,7 +12,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ProductCreateComponent implements OnInit {
   productForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private router:Router,
+              private toastr: ToastrService) {
     this.productForm = this.fb.group(
       {
         name: ['', Validators.required],
@@ -25,7 +30,17 @@ export class ProductCreateComponent implements OnInit {
   }
 
   AddProduct(){
-    console.log(this.productForm)
+
+    const PRODUCT: Product = {
+      name: this.productForm.get('name')?.value,
+      description: this.productForm.get('description')?.value,
+      category: this.productForm.get('category')?.value,
+      price: this.productForm.get('price')?.value,
+      amount: this.productForm.get('amount')?.value,
+    }
+    console.log(PRODUCT);
+    this.toastr.success('Product registered successfully!', 'Registered product');
+    this.router.navigate(['/']);
   }
 
 }
